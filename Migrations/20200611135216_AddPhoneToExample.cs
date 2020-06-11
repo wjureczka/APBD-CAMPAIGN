d8fc90ad26ffa8506742cad5526747919794fcc7
Wojciech Jureczka <wjureczka@gmail.com>
2020-06-11 15:52:44 +0200

    Add new example for migration

---
+++ Migrations/20200611135216_AddPhoneToExample.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace APBD_CAMPAIGN.Migrations
+{
+    public partial class AddPhoneToExample : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Campaign",
+                keyColumn: "IdCampaign",
+                keyValue: 1,
+                columns: new[] { "EndDate", "StartDate" },
+                values: new object[] { new DateTime(2020, 6, 11, 15, 52, 16, 476, DateTimeKind.Local).AddTicks(3757), new DateTime(2020, 6, 11, 15, 52, 16, 469, DateTimeKind.Local).AddTicks(7135) });
+
+            migrationBuilder.UpdateData(
+                table: "Client",
+                keyColumn: "IdClient",
+                keyValue: 1,
+                column: "Phone",
+                value: "123123123");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Campaign",
+                keyColumn: "IdCampaign",
+                keyValue: 1,
+                columns: new[] { "EndDate", "StartDate" },
+                values: new object[] { new DateTime(2020, 6, 11, 15, 31, 10, 687, DateTimeKind.Local).AddTicks(1501), new DateTime(2020, 6, 11, 15, 31, 10, 680, DateTimeKind.Local).AddTicks(3942) });
+
+            migrationBuilder.UpdateData(
+                table: "Client",
+                keyColumn: "IdClient",
+                keyValue: 1,
+                column: "Phone",
+                value: null);
+        }
+    }
+}
